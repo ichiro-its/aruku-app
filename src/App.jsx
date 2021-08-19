03a4e4340e6d4ca5416bb09d8045591cef38c3f2
JayantiTA <jayjayanti18@gmail.com>
2021-08-20 00:16:47 +0700

    feat: add header file

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,28 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import MuiAppBar from "@material-ui/core/AppBar";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Grid from "@material-ui/core/Grid";
-import IconButton from "@material-ui/core/IconButton";
 import TextField from "@material-ui/core/TextField";
-import MuiToolbar from "@material-ui/core/Toolbar";
 import MuiTypography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import MUIDataTable from "mui-datatables";
+import Header from "./components/Header";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-  flexGrow: 1,
+    flexGrow: 1,
   },
   button: {
-    margin: theme.spacing(1),
+    margin: "10px",
   },
   table: {
-    marginTop: "10px",
+    marginBottom: "10px",
   },
+  title: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+  }
 }));
 
 const columns = [
@@ -39,6 +41,7 @@ const columns = [
           value={value}
           control={<TextField value={value} />}
           onChange={(event) => updateValue(event.target.value)}
+
         />
       ),
     },
@@ -46,55 +49,55 @@ const columns = [
 ];
 
 const ratioData = [
-  { name: "forward_hip_comp_ratio", value: 0.0 },
-  { name: "dsp_comp_ratio", value: 0.0 },
-  { name: "period_time", value: 0.0 },
-  { name: "swing_right_left", value: 0.0 },
-  { name: "dsp_ratio", value: 0.0 },
-  { name: "period_comp_ratio", value: 0.0 },
-  { name: "swing_up_down", value: 0.0 },
-  { name: "arm_swing_gain", value: 0.0 },
-  { name: "backward_hip_comp_ratio", value: 0.0 },
-  { name: "foot_comp_ratio", value: 0.0 },
-  { name: "foot_height", value: 0.0 },
-  { name: "move_accel_ratio", value: 0.0 },
-  { name: "foot_accel_ratio", value: 0.0 },
+  { name: "forward_hip_comp_ratio", value: 0.0},
+  { name: "dsp_comp_ratio", value: 0.0},
+  { name: "period_time", value: 810.0},
+  { name: "swing_right_left", value: 15.0},
+  { name: "dsp_ratio", value: -0.12},
+  { name: "period_comp_ratio", value: 0.0},
+  { name: "swing_up_down", value: -1.0},
+  { name: "arm_swing_gain", value: 0.2},
+  { name: "backward_hip_comp_ratio", value: 0.0},
+  { name: "foot_comp_ratio", value: 0.0},
+  { name: "foot_height", value: 30.0},
+  { name: "move_accel_ratio", value: 1.0},
+  { name: "foot_accel_ratio", value: 0.2}
 ];
 
 const offsetData = [
-  { name: "x_offset", value: 0.0 },
-  { name: "y_offset", value: 0.0 },
-  { name: "z_offset", value: 0.0 },
-  { name: "roll_offset", value: 0.0 },
-  { name: "hip_pitch_offset", value: 0.0 },
-  { name: "pitch_offset", value: 0.0 },
-  { name: "yaw_offset", value: 0.0 },
+  { name: "y_offset", value: 0.0},
+  { name: "roll_offset", value: 1.0},
+  { name: "hip_pitch_offset", value: 23.0},
+  { name: "pitch_offset", value: -0.4},
+  { name: "yaw_offset", value: 0.1},
+  { name: "z_offset", value: 25.0},
+  { name: "x_offset", value: 40.0}
 ];
 
 const balanceData = [
-  { name: "balance_knee_gain", value: 0.0 },
-  { name: "balance_ankle_pitch_gain", value: 0.0 },
-  { name: "balance_hip_roll_gain", value: 0.0 },
-  { name: "balance_ankle_roll_gain", value: 0.0 },
+  { name: "balance_knee_gain", value: 0.3},
+  { name: "balance_ankle_pitch_gain", value: 0.3},
+  { name: "balance_hip_roll_gain", value: 0.2},
+  { name: "balance_ankle_roll_gain", value: 0.2}
 ];
 
 const pidData = [
-  { name: "p_gain", value: 0.0 },
-  { name: "i_gain", value: 0.0 },
-  { name: "d_gain", value: 0.0 },
+  { name: "p_gain", value: 10},
+  { name: "i_gain", value: 2},
+  { name: "d_gain", value: 3}
 ];
 
 const odometryData = [
-  { name: "ry_coefficient", value: 0.0 },
-  { name: "fx_coefficient", value: 0.0 },
-  { name: "ly_coefficient", value: 0.0 },
+  { name: "ry_coefficient", value: 5.0},
+  { name: "fx_coefficient", value: 5.85},
+  { name: "ly_coefficient", value: 5.0}
 ];
 
 const kinematicData = [
-  { name: "thigh_length", value: 0.0 },
-  { name: "calf_length", value: 0.0 },
-  { name: "ankle_length", value: 0.0 },
-  { name: "leg_length", value: 0.0 },
+  { name: "thigh_length", value: 199.0},
+  { name: "calf_length", value: 195.0},
+  { name: "ankle_length", value: 59.7},
+  { name: "leg_length", value: 394}
 ];
 
 const options_1 = {
@@ -105,10 +108,6 @@ const options_1 = {
   print: false,
   search: true,
   selectableRows: "single",
-  sortOrder: {
-    name: "ID",
-    direction: "asc",
-  },
   selectableRowsOnClick: false,
   selectableRowsHideCheckboxes: true,
 };
@@ -121,10 +120,6 @@ const options_2 = {
   print: false,
   search: true,
   selectableRows: "single",
-  sortOrder: {
-    name: "ID",
-    direction: "asc",
-  },
   selectableRowsOnClick: false,
   selectableRowsHideCheckboxes: true,
 };
@@ -137,10 +132,6 @@ const options_3 = {
   print: false,
   search: true,
   selectableRows: "single",
-  sortOrder: {
-    name: "ID",
-    direction: "asc",
-  },
   selectableRowsOnClick: false,
   selectableRowsHideCheckboxes: true,
 };
@@ -150,75 +141,68 @@ function App() {
   
   return (
     <div className={classes.root}>
-      <MuiAppBar position="static">
-        <MuiToolbar>
-          {/* <img src="../logo.png" width="100" height="100"/> */}
-          <MuiTypography variant="h5">Aruku App</MuiTypography>
-        </MuiToolbar>
-      </MuiAppBar>
+      <Header />
       <Card>
-        <MuiToolbar>
-          <MuiTypography variant="h6">Walking Settings</MuiTypography>
-        </MuiToolbar>
+        <Grid item xs={12} className={classes.title}>
+          <MuiTypography variant="h6" >
+            Walk Settings
+          </MuiTypography>
+        </Grid>
         <CardContent>
           <Grid container spacing={3}>
             <Grid item xs={6}>
               <MUIDataTable
+                className={classes.table}
                 title="Ratio"
                 data={ratioData}
                 columns={columns}
                 options={options_1}
               />
-              <div style={{ marginTop: 10 }}>
-                <MUIDataTable
-                  title="Balance"
-                  data={balanceData}
-                  columns={columns}
-                  options={options_2}
-                />
-              </div>
-              <div style={{ marginTop: 10 }}>
-                <MUIDataTable
+              <MUIDataTable
+                className={classes.table}
+                title="Balance"
+                data={balanceData}
+                columns={columns}
+                options={options_2}
+              />
+              <MUIDataTable
+                className={classes.table}
                 title="PID"
                 data={pidData}
                 columns={columns}
                 options={options_3}
-                />
-              </div>
+              />
             </Grid>
             <Grid item xs={6}>
               <MUIDataTable
+                className={classes.table}
                 title="Offset"
                 data={offsetData}
                 columns={columns}
                 options={options_1}
               />
-              <div style={{ marginTop: 10 }}>
-                <MUIDataTable
-                  title="Kinematic"
-                  data={kinematicData}
-                  columns={columns}
-                  options={options_2}
-                />
-              </div>
-              <div style={{ marginTop: 10 }}>
-                <MUIDataTable
-                  title="Odometry"
-                  data={odometryData}
-                  columns={columns}
-                  options={options_3}
-                />
-              </div>
+              <MUIDataTable
+                className={classes.table}
+                title="Kinematic"
+                data={kinematicData}
+                columns={columns}
+                options={options_2}
+              />
+              <MUIDataTable
+                className={classes.table}
+                title="Odometry"
+                data={odometryData}
+                columns={columns}
+                options={options_3}
+              />
             </Grid>
           </Grid>
-          <div style={{ margin: 10 }}>
-          <Button variant="contained" color="primary">
+          <Button variant="contained" color="primary" className={classes.button}>
             Save
           </Button>
-          <Button variant="contained" color="secondary">
+          <Button variant="contained" color="secondary" className={classes.button}>
             Reload
           </Button>
-          </div>
         </CardContent>
       </Card>
     </div>
